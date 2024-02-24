ebad52b17935d62ddf31f435d76eeb57b7ff08cc
sssujijl <leesuji1998@naver.com>
2024-02-24 19:15:54 +0900

    feat: feedback router추가

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import errorHandlerMiddleware from "../middlewares/error-handler.Middleware.js";
-import router from "./routes/index.js";
+import errorHandlerMiddleware from "./middlewares/error-handler.Middleware.js";
+import feedbacksRouter from './src/routes/feedbacks.routes.js';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -12,12 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", router);
+app.use('/feedback/:projectId', feedbacksRouter);
 
 app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, "번 포트로 서버가 열렸어요! http://localhost:3000/");
 });
-
-export default router;
