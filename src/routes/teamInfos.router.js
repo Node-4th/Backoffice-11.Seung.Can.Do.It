32db4a66168104d9438d962d4cc82c62e76f03ac
sssujijl <leesuji1998@naver.com>
2024-02-27 18:43:31 +0900

    front

--- src/routes/teamInfos.router.js
+++ src/routes/teamInfos.router.js
@@ -3,6 +3,7 @@ import { prisma } from '../models/index.js';
 import { TeamInfosController } from '../controllers/teamInfos.controller.js';
 import { TeamInfosService } from '../services/teamInfos.service.js';
 import { TeamInfosRepository } from '../repositories/teamInfos.repository.js';
+import authMiddleware from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 const teamInfosRepository = new TeamInfosRepository(prisma);
@@ -10,15 +11,15 @@ const teamInfosService = new TeamInfosService(teamInfosRepository);
 const teamInfosController = new TeamInfosController(teamInfosService);
 
 // 팀정보 상세 조회
-router.get('/:teamInfoId', teamInfosController.findTeamInfos);
+router.get('/:teamInfoId',authMiddleware, teamInfosController.findTeamInfos);
 
 // 팀정보 생성
-router.post('/:teamId', teamInfosController.createTeamInfos);
+router.post('/:teamId',authMiddleware, teamInfosController.createTeamInfos);
 
 // 팀정보 수정
-router.put('/:teamInfoId', teamInfosController.editTeamInfos);
+router.put('/:teamInfoId',authMiddleware, teamInfosController.editTeamInfos);
 
 // 팀정보 삭제
-router.delete('/:teamInfoId', teamInfosController.deleteTeamInfos);
+router.delete('/:teamInfoId',authMiddleware, teamInfosController.deleteTeamInfos);
 
 export default router;
