e79d432477b0b0c4298d012b9d665d4c473acdd7
ratempty <rkdekqls258@naver.com>
2024-02-28 15:02:46 +0900

    Feat : 이메일발송기능 , 해당 이메일로 가입 기능

--- src/routes/email.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Prisma } from "@prisma/client";
-import Jwt from "jsonwebtoken";
-
-const router = express.Router();
-
-router.get("/auth/token", async (req, res) => {});
-
-export default router;
