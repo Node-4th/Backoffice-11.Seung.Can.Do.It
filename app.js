c068323ce7a6452e20d575ac790373365ba55163
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-02-29 01:39:35 +0900

    <Merge>

--- app.js
+++ app.js
@@ -100,17 +100,17 @@ app.get("/admins/til", async (req, res, next) => {
 });
 
 ////////////////////////////////
-const job = schedule.scheduleJob("*/5 * * * * *", async () => {
-  const response = await axios.post(
-    "http://localhost:3000/projects/submit/slack",
-    {
-      category: "PERSONAL_PROJECT",
-      start: "2024-02-02T15:00:00.000Z",
-      end: "2024-02-29T15:00:00.000Z",
-      classId: 1,
-    },
-  );
-});
+// const job = schedule.scheduleJob("*/5 * * * * *", async () => {
+//   const response = await axios.post(
+//     "http://localhost:3000/projects/submit/slack",
+//     {
+//       category: "PERSONAL_PROJECT",
+//       start: "2024-02-02T15:00:00.000Z",
+//       end: "2024-02-29T15:00:00.000Z",
+//       classId: 1,
+//     },
+//   );
+// });
 
 app.listen(PORT, () => {
   console.log(PORT, "번 포트로 서버가 열렸어요! http://localhost:3000/");
