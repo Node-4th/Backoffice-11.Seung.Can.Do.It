e3d135efcbb225cd7c6b6364aa8b30e59b4a83d2
sssujijl <leesuji1998@naver.com>
2024-02-29 10:53:38 +0900

    fix

--- app.js
+++ app.js
@@ -161,17 +161,17 @@ rule.hour = [9, 12, 21];
 rule.minute = 0;
 rule.tz = "Asia/Seoul";
 
-const job = schedule.scheduleJob(rule, async () => {
-  const response = await axios.post(
-    "http://localhost:3000/projects/submit/slack",
-    {
-      category: "PERSONAL_PROJECT",
-      start: "2024-02-02T15:00:00.000Z",
-      end: "2024-02-29T15:00:00.000Z",
-      classId: 1,
-    },
-  );
-});
+// const job = schedule.scheduleJob(rule, async () => {
+//   const response = await axios.post(
+//     "http://localhost:3000/projects/submit/slack",
+//     {
+//       category: "PERSONAL_PROJECT",
+//       start: "2024-02-02T15:00:00.000Z",
+//       end: "2024-02-29T15:00:00.000Z",
+//       classId: 1,
+//     },
+//   );
+// });
 
 ////////////////////////////////
 const rule = new schedule.RecurrenceRule();
