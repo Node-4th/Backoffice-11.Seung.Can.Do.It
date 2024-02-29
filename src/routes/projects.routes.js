c573be6be0cf265e5953ebabf2aa0df8706cfb82
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-02-29 10:58:09 +0900

    <10:58 Merge>

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import slackSender from "../utils/slackSender.js";
 
 /**PrismaORM -> 3계층 의존성 주입 */
 import { prisma } from "../models/index.js";
@@ -29,53 +28,21 @@ router.get(
 router.post("/", authMiddleware, projectsController.createProject);
 router.put("/:projectId", authMiddleware, projectsController.updateProject);
 router.delete("/:projectId", authMiddleware, projectsController.deleteProject);
-<<<<<<< HEAD
-router.post("/submit/slack", async (req, res, next) => {
-  try {
-    const { category, start, end, classId } = req.body;
-
-=======
-
-// router.post(
-//   "/submit/slack",
-//   authMiddleware,
-//   projectsController.notSubmitUserSendSlack,
-//   async (req, res, next) => {
-//     try {
-//       const text = req.notSubmitUsers;
-//       let nameArr = text.map((item) => item.name);
-//       let resultString = nameArr.join(", ");
-//       console.log("resultString: ", resultString);
-//       await slackSender(resultString);
-//       return res.status(200).json({ message: "슬랙으로 메세지 발송완료" });
-//     } catch (error) {
-//       next(error);
-//     }
-//   },
-// );
 
 router.post("/submit/slack", async (req, res, next) => {
   try {
     const { category, start, end, classId } = req.body;
->>>>>>> 65d6b810a736f3834874d10739ddee0a4678c8bd
     const notSubmitUsers = await projectsRepository.getAllNotSubmitUser(
       category,
       start,
       end,
       classId,
     );
-<<<<<<< HEAD
 
-=======
->>>>>>> 65d6b810a736f3834874d10739ddee0a4678c8bd
     const text = notSubmitUsers;
     let nameArr = text.map((item) => item.name);
     let resultString = nameArr.join(", ");
     await slackSender(resultString);
-<<<<<<< HEAD
-
-=======
->>>>>>> 65d6b810a736f3834874d10739ddee0a4678c8bd
     return res.status(200).json({ message: "슬랙으로 메세지 발송완료" });
   } catch (error) {
     next(error);
